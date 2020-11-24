31735ad39f213f4d48ab72ef162e0cfa08675543
Arjun Khurana <arjunkh99@gmail.com>
2020-11-24 10:04:16 -0600

    finalized contiguous

--- ContiguousSystem.java
+++ ContiguousSystem.java
@@ -6,6 +6,7 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.io.ObjectInputStream;
 
 public class ContiguousSystem extends FileSystem {
@@ -23,15 +24,6 @@ public class ContiguousSystem extends FileSystem {
         int start = 0;
         int length = 0;
         boolean found = false;
-
-        // for (FileEntry e : ft.table) {
-        //     if (String.valueOf(e.name).equals(name)) {
-        //         found = true;
-        //         start = e.start;
-        //         length = e.length;
-        //         break;
-        //     }
-        // }
 
         for (int i = 0; i < ft.table.length; i++) {
             if (ft.table[i] == null) {
@@ -85,16 +77,88 @@ public class ContiguousSystem extends FileSystem {
         }
     }
 
+    private int addToFileTable(String filename, byte where, byte length) throws Exception{
+        byte[] ftBytes = this.memory.read(0);
+        ByteArrayInputStream in = new ByteArrayInputStream(ftBytes);
+        ObjectInputStream is = new ObjectInputStream(in);
+        FileTable ft = (FileTable)is.readObject();
+
+        boolean ftSpace = false;
+
+        for (int i = 0; i < ft.table.length; i++) {
+            if (ft.table[i] == null) {
+                ft.table[i] = new FileEntry(filename, (byte)where, (byte)length);
+                ftSpace = true;
+                break;
+            }
+        } 
+
+        if (!ftSpace) {
+            System.out.println("No space found in filetable.");
+            return 1;
+        }
+        
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(ft);
+        oos.flush();
+        ftBytes = bos.toByteArray();
+        this.memory.write(0, ftBytes);
+        return 0;
+    }
+
     public int diskToSim(Path path, String filename) throws Exception {
         byte[] data = Files.readAllBytes(path);
         if (data.length >= 512) {
-            System.out.println("TODO");
+            int blockLength = 512;
+            int leftover = data.length % blockLength;
+
+            int numBlocks = data.length/blockLength + (leftover > 0 ? 1 : 0);
+            byte[][] blockData  = new byte[numBlocks][blockLength];
+            for (int i = 0; i < (leftover > 0 ? numBlocks - 1 : numBlocks); i++) {
+                blockData[i] = Arrays.copyOfRange(data, i*blockLength, i*blockLength + blockLength);
+            }
+            if (leftover > 0) {
+                blockData[numBlocks - 1] = Arrays.copyOfRange(data, (numBlocks-1)*blockLength, (numBlocks-1)*blockLength + leftover); 
+            }
+
+            byte[] bitmap = this.memory.read(1);
+            int where = 0;
+
+            for (int i = 0; i < bitmap.length; i++) {
+                boolean foundSpace = true;
+                if (bitmap[i] == 0) {
+                    for (int j = i; j < i + numBlocks; j++) {
+                        if (bitmap[j] == 1) {
+                            foundSpace = false;
+                            break;
+                        }
+                    }
+                    if (foundSpace) {
+                        where = i;
+                        break;
+                    }
+                }
+            }
+
+            if (where == 0) {
+                System.out.println("No space found on disk.");
+                return 1;
+            }
+
+            for (int i = where, j = 0; i < where + numBlocks; i++, j++) {
+                this.memory.write(i, blockData[j]);
+            }
+
+            if (this.addToFileTable(filename, (byte)where, (byte)numBlocks) != 0) {
+                return 1;
+            }
+
         } else {
             int where = 0;
             byte[] bitmap = this.memory.read(1);
             for (int i = 0; i < bitmap.length; i++) {
                 if (bitmap[i] == 0) {
-                    System.out.println(bitmap[i]);
                     this.memory.write(i, data);
                     where = i;
                     break;
@@ -105,34 +169,10 @@ public class ContiguousSystem extends FileSystem {
                 System.out.println("No space found on disk.");
                 return 1;
             }
-            
-            // System.out.println(data.length);
-            byte[] ftBytes = this.memory.read(0);
-            ByteArrayInputStream in = new ByteArrayInputStream(ftBytes);
-            ObjectInputStream is = new ObjectInputStream(in);
-            FileTable ft = (FileTable)is.readObject();
 
-            boolean ftSpace = false;
-
-            for (int i = 0; i < ft.table.length; i++) {
-                if (ft.table[i] == null) {
-                    ft.table[i] = new FileEntry(filename, (byte)where, (byte)1);
-                    ftSpace = true;
-                    break;
-                }
-            } 
-
-            if (!ftSpace) {
-                System.out.println("No space found in filetable.");
+            if (this.addToFileTable(filename, (byte)where, (byte)1) != 0) {
                 return 1;
             }
-            
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(bos);
-            oos.writeObject(ft);
-            oos.flush();
-            ftBytes = bos.toByteArray();
-            this.memory.write(0, ftBytes);
         }
         return 0;
     }
@@ -146,15 +186,6 @@ public class ContiguousSystem extends FileSystem {
         int start = 0;
         int length = 0;
         boolean found = false;
-
-        // for (FileEntry e : ft.table) {
-        //     if (String.valueOf(e.name).equals(name)) {
-        //         found = true;
-        //         start = e.start;
-        //         length = e.length;
-        //         break;
-        //     }
-        // }
 
         for (int i = 0; i < ft.table.length; i++) {
             if (ft.table[i] == null) {
@@ -178,7 +209,10 @@ public class ContiguousSystem extends FileSystem {
         OutputStream fileOut = new FileOutputStream(outputFile);
 
         if (length > 1) {
-            System.out.println("TODO");
+            for (int i = 0; i < length; i++) {
+                byte[] data = this.memory.read(start + i);
+                fileOut.write(data);
+            }
         } else {
             byte[] data = this.memory.read(start);
             fileOut.write(data);
@@ -193,15 +227,6 @@ public class ContiguousSystem extends FileSystem {
         ByteArrayInputStream in = new ByteArrayInputStream(ftBytes);
         ObjectInputStream is = new ObjectInputStream(in);
         FileTable ft = (FileTable)is.readObject();
-
-        // for (FileEntry e : ft.table) {
-        //     if (String.valueOf(e.name).equals(name)) {
-        //         found = true;
-        //         start = e.start;
-        //         length = e.length;
-        //         break;
-        //     }
-        // }
 
         int start = 0;
         int length = 0;
